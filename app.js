7bc1af42b28610e4a69cd8afd692322b361aa661
Pablo Fernández <papicota1979@gmail.com>
2018-12-28 19:45:53 +0100

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables
 var app = express();
+
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 // Conexión a la base de datos
@@ -15,15 +27,12 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res )
     console.log('Base de datos: \x1b[31m%s\x1b[0m', 'online');
 })
 
-
 // Rutas
-app.get('/', ( req, res, next ) => {
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
+
 
 
 // Escuchar peticiones
